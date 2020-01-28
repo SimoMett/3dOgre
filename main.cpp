ec7c5f0ce483c90a4335f353717d9ae55e03a7e8
matteo <matt.simonetti@hotmail.com>
2020-01-28 18:02:03 +0100

    Finally got to display a fish

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include <Ogre.h>
 #include <OgreRenderWindow.h>
 #include <OgreRoot.h>
-#include "MainApp.h"
+#include <OgreWindowEventUtilities.h>
+#include <RTShaderSystem/OgreRTShaderSystem.h>
 
 using namespace Ogre;
 
@@ -13,17 +14,49 @@ using std::endl;
 
 int main(int argc, char ** argv)
 {
-    MainApp app("Test");
-    app.start();
 
-    /*SceneManager* sceneManager = root->createSceneManager();
-    sceneManager->setAmbientLight(Ogre::ColourValue(0.0, 0.0, 0.0));
+    Root * root=new Root("","","");
+    SceneManager * scnMgr=root->createSceneManager();
 
-    // Create a new camera
-    Camera* camera = sceneManager->createCamera("Camera");
-    camera->setPosition(Ogre::Vector3(0,0,15));
-    camera->lookAt(Ogre::Vector3(0,0,0));
-    camera->setNearClipDistance(5);*/
+    root->loadPlugin("/usr/lib/OGRE/RenderSystem_GL");
+
+    root->setRenderSystem(root->getAvailableRenderers()[0]);
+
+    RenderWindow * window=root->initialise(true);
+
+    // register our scene with the RTSS
+    //RTShader::ShaderGenerator* shadergen = Ogre::RTShader::ShaderGenerator::getSingletonPtr();
+    //shadergen->addSceneManager(scnMgr);
+    // without light we would just get a black screen
+    Ogre::Light* light = scnMgr->createLight("MainLight");
+    Ogre::SceneNode* lightNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    lightNode->setPosition(0, 10, 15);
+    lightNode->attachObject(light);
+    // also need to tell where we are
+    Ogre::SceneNode* camNode = scnMgr->getRootSceneNode()->createChildSceneNode();
+    camNode->setPosition(0, 0, 15);
+    camNode->lookAt(Ogre::Vector3(0, 0, -1), Ogre::Node::TS_PARENT);
+    // create the camera
+    Ogre::Camera* cam = scnMgr->createCamera("myCam");
+    cam->setNearClipDistance(5); // specific to this sample
+    cam->setAutoAspectRatio(true);
+    camNode->attachObject(cam);
+    // and tell it to render into the main window
+    window->addViewport(cam);
+    // finally something to render
+
+    ResourceGroupManager::getSingleton().addResourceLocation("/usr/share/OGRE/Media/models","FileSystem","General",true);
+
+    Ogre::Entity* ent = scnMgr->createEntity("fish.mesh");
+    ent->setDisplaySkeleton(true);
+    Ogre::SceneNode* node = scnMgr->getRootSceneNode()->createChildSceneNode();
+    node->attachObject(ent);
+
+
+    while(true)
+    {
+        root->renderOneFrame();
+    }
 
     return 0;
 }
