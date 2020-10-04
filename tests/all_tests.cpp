5638e2f647442cf0a36736a38c5226e237259dc4
matteo <matt.simonetti@hotmail.com>
2020-10-04 17:05:18 +0200

    Implementing tests

--- tests/all_tests.cpp
+++ tests/all_tests.cpp
@@ -1,10 +1,35 @@
 //
 // Created by matteo on 04/10/20.
 //
+
+#include <fstream>
+#include <exception>
+
 #define BOOST_TEST_MAIN
 #include <boost/test/unit_test.hpp>
+#include <boost/exception/exception.hpp>
+
+#include "../src/dff/DffSection.h"
 
 BOOST_AUTO_TEST_CASE(my_test)
 {
-    printf("test completed\n");
+    using namespace std;
+    try
+    {
+        ifstream dffFile;
+        dffFile.open("SimpleCube.dff");
+            if(!dffFile.good()) throw "file not good";
+
+        unsigned int size=dffFile.tellg();
+        char * buffer=new char[size];
+        dffFile.read(buffer,size);
+
+
+        dffFile.close();
+        delete[] buffer;
+    }
+    catch (exception & e)
+    {
+        cerr << e.what() << endl;
+    }
 }
